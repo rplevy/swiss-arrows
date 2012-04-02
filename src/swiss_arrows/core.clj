06c3c7e1ff79cb67b8bec980997fdbd55e0e4fed
Robert Levy <r.p.levy@gmail.com>
2012-04-01 21:05:24 -0400

    diamond wand with support for hash literals, vectors, and all quoted forms

--- src/swiss_arrows/core.clj
+++ src/swiss_arrows/core.clj
@@ -1,24 +1,54 @@
 (ns swiss-arrows.core)
 
 (defmacro -<>
-  "the 'diamond wand': pass a needle through variably positioned holes"
+  "the 'diamond wand': pass a needle through variably positioned holes
+   the <> hole form is not recursive, it only works at the top level.
+   also, it works with hash literals, vectors, and quoted forms"
   ([x] x)
   ([x form]
-     (let [form (if (map? form) (cons 'hash-map (apply concat form)) form)
-           [pre _ post] (partition-by (partial = '<>) form)
-           result (concat pre [x] post)]
-       (if (vector? form) (vec result) result)))
+     (let [[process-result form] (if (= 'quote (first form))
+                                   [(fn [r] `(quote ~r)) (first (next form))]
+                                   [identity             form])
+           [process-result form] (if (map? form)
+                                   [(comp process-result
+                                          (partial apply hash-map))
+                                    (apply concat form)]
+                                   [process-result       form])
+           process-result (if (vector? form)
+                            (comp process-result vec)
+                            process-result)
+           [pre _ post] (partition-by (partial = '<>) form)]
+       (process-result (concat pre [x] post))))
   ([x form & forms]
      `(-<> (-<> ~x ~form) ~@forms)))
 
+#_(defmacro <<-
+    "the 'back-arrow': suggested by Stephen Compall in response to a certain
+     recently announced Clojure library"
+    [])
 
-#_(defmacro <<- []
-  "the 'double-back-arrow': "
-  )
+#_(defmacro -<
+    "'the divining rod': branch one result into multiple flows" \
+    [])
 
-  ;; TODO: macro for spliting into multiple streams
+#_(defmacro -<<
+    "'the double divining rod': analog of ->> for divining rod"
+    [])
 
+#_(defmacro -<><
+    "'the diamond fishing rod': analog of -<> for divining rod"
+    [])
 
-;; TODO: <<-
+#_(defmacro -<:p
+    "parallel divining rod"
+    [])
+
+#_(defmacro -<<:p
+    "parallel divining rod, double style"
+    [])
+
+#_(defmacro -<><:p
+    "parallel diamond fishing rod"
+    [])
 
 
--- test/swiss_arrows/test/core.clj
+++ test/swiss_arrows/test/core.clj
@@ -2,7 +2,8 @@
   (:use [swiss-arrows.core]
         [midje.sweet]))
 
-(facts "the diamond wand"
+(facts
+ "the diamond wand"
 
  (-<> (first [1])) => 1
  
@@ -16,14 +17,89 @@
               (-<> 10
                    [7 8 9 <> 11 12]
                    (cons 6 <>))))
- => (range -1 12)
+ => (range -1 13)
+
+ ;; vector 
+ (-<> 10 [1 2 3 <> 4 5])
+ => [1 2 3 10 4 5]
+
+ (-<> 10 [1 2 3 <> 4 5])
+ => vector?
+
+ ;; quoted vector
+ (-<> 10 '[1 2 a <> 4 5])
+ => [1 2 'a 10 4 5]
+
+ ;; quoted list
+ (-<> 10 '(1 2 a <> 4 5))
+ => '(1 2 a 10 4 5)
  
- (-<> 'foo {:a <> :b 'bar}) => {:a 'foo :b 'bar})
+ ;; map
+ (-<> 'foo {:a <> :b 'bar})
+ => {:a 'foo :b 'bar}
+
+ ;; quoted map
+ (-<> foo '{:a a :b <>})
+ => {:a 'a :b 'foo})
  
-#_(facts "the double-back-arrow"
-   
-   (<<-
-    (let [x nonsense])
-    (if early-term thing)
-    (let [more blah])
-    '[more inner forms…]))
+#_(fact
+ "back-arrow"
+ 
+ (<<-
+  (let [x 'nonsense] x)
+  (if-not x 'foo)
+  (let [more 'blah]
+    more))
+ 
+ =>
+
+ (let [more 'blah]
+   more
+   (if-not more 'foo
+           (let [x 'nonsense] x))))
+ 
+
+#_(facts
+ "divining rods"
+
+ (-< (+ 1 2)
+     (list 2)
+     (list 3)
+     (list 4))
+ =>
+ '[(3 2) (3 3) (3 4)]
+
+ (-<< (+ 1 2)
+      (list 2 1)
+      (list 5 7)
+      (list 9 4))
+ =>
+ '[(2 1 3) (5 7 3) (9 4 3)]
+
+ (-<>< (+ 1 2)
+       (list <> 2 1)
+       (list 5 <> 7)
+       (list 9 4 <>))
+ =>
+ '[(3 2 1) (5 3 7) (9 4 3)]
+
+ (-<:p (+ 1 2)
+       (list 2)
+       (list 3)
+       (list 4))
+ =>
+ '[(3 2) (3 3) (3 4)]
+
+ (-<<:p (+ 1 2)
+        (list 2 1)
+        (list 5 7)
+        (list 9 4))
+ =>
+ '[(2 1 3) (5 7 3) (9 4 3)]
+
+ (-<><:p (+ 1 2)
+         (list <> 2 1)
+         (list 5 <> 7)
+         (list 9 4 <>))
+ =>
+ '[(3 2 1) (5 3 7) (9 4 3)])
