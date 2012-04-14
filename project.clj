4470fc42f4b2c4288b481868f90853c372231ec1
Robert Levy <r.p.levy@gmail.com>
2012-04-14 02:49:57 -0400

    bump patch version for refactor

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swiss-arrows "0.2.0"
+(defproject swiss-arrows "0.2.1"
   :description "more arrow macros"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]]
