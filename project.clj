1745007f23e193618b94d5f23e92f296cfee188b
Robert Levy <r.p.levy@gmail.com>
2012-06-14 00:03:12 -0400

    update project.clj to lein 2 syntax

--- project.clj
+++ project.clj
@@ -1,4 +1,12 @@
 (defproject swiss-arrows "0.3.0"
   :description "more arrow macros"
+  :url "https://clojars.org/swiss-arrows"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[midje "1.3.0"]])
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
+
+
+
+
